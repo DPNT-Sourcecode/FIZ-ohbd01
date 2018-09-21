52342e0ddb857a0170cd2cd93317af4f5a5e0759
sbx_user1060 <sbx_user1060@ip-10-129-95-135.eu-west-2.compute.internal>
2018-09-21 23:13:27 +0000

    Fri Sep 21 23:13:27 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -1,8 +1,36 @@
+package befaster.solutions.FIZ;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-/**
- * Created by amin on 22/09/2018.
- */
+
 public class FizzBuzzSolutionTest {
 
+    private FizzBuzzSolution classToTest = new FizzBuzzSolution();
+
+    @Test
+    public void shouldReturnFizzGivenNumberMultipleOfThree(){
+        // Given
+        int number = 6;
+
+        // When
+        String actual = classToTest.fizzBuzz(number);
+
+        // Then
+        assertThat(actual, is("fizz"));
+    }
+
+    @Test
+    public void shouldReturnBuzzGivenNumberMultipleOfFive(){
+        // Given
+        int number = 10;
+
+        // When
+        String actual = classToTest.fizzBuzz(number);
+
+        // Then
+        assertThat(actual, is("buzz"));
+    }
 }
