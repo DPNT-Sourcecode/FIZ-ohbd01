b8caa196ca78b7b25d53637c07cd94805d0571fe
sbx_user1060 <sbx_user1060@ip-10-129-95-135.eu-west-2.compute.internal>
2018-09-21 22:55:41 +0000

    Fri Sep 21 22:55:41 UTC 2018

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,8 +1,22 @@
 package befaster.solutions.HLO;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 
 public class HelloSolutionTest {
 
+    @Test
+    public void shouldReturnAMesage(){
+        // Given
+
+        // When
+        String name = "John";
+        String message = new HelloSolution().hello(name);
+
+        // Then
+        assertThat(message, is("Hello " + name));
+    }
 }
